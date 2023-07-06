ed2c2c999339cb6fa2da51172f36e4f28049fa55
Ali Hassan <hassanali8812@gmail.com>
2023-07-06 22:49:09 +0500

    Updates

--- index.js
+++ index.js
@@ -69,9 +69,9 @@ class Simulator{
             }
         };
         this.startObj = {
-            x: 0,
-            y: 0,
-            z: 0,
+            x: 1,
+            y: 1,
+            z: 1,
         }
         this.endObj = {
             x: 40,
